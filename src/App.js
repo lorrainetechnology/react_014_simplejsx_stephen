c305b5b1349803e3df6c634f2c66a178f1d55e10
lorrainetechnology <lorrainetechnology@gmail.com>
2022-05-15 20:46:14 +0530

    simple jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import { ReactDOM } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const buttonText = { text: 'Click Me' };
+  const labelText = 'Enter name:';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text"/>
+      <button style={{backgroundColor: 'blue', color: 'white'}}>
+        {buttonText.text}
+      </button>
     </div>
   );
-}
+};
 
 export default App;
